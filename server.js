d3c774f080349bb39d39b59630bc7f1d6b2d41e3
Rok Garbas <rok@garbas.si>
2014-04-13 04:48:30 +0100

    cleanup
    - switch from optimist to yargs
    - switch from connect to express
    - added socket.io
    - using jade view for generating express views
    - adding initiall bower.json

--- server.js
+++ server.js
@@ -1,36 +1,85 @@
-var optimist = require('optimist'),
-    argv = optimist
-      .usage('$0 [options]')
-      .alias('p', 'port')
-        .default('p', 8000)
-        .describe('p', 'port on which weecloud should run')
-      .alias('h', 'help')
-        .describe('h', 'Display the usage')
-      .argv
+//
+// --- CLI ---
+//
+
+var timestap = function(original) {
+      return function(msg) {
+        original('%s - %s', (new Date()).toISOString(), msg);
+      };
+    },
+    log = timestap(console.log),
+    warn = timestap(console.warn);
+
+console.log = console.warn = function() {};
+
+var yargs = require('yargs')
+  .wrap(process.stdout.columns)
+  .usage('$0 [options]')
+  .options('config', {
+    alias: 'c',
+    describe: 'Options provides in JSON file.'
+  })
+  .config('c')
+  .options('username', {
+    describe: 'Username.',
+    alias: 'u'
+  })
+  .options('password', {
+    describe: 'Password.',
+    alias: 'p'
+  })
+  .demand(['username', 'password'])
+  .describe('cookie-secret', 'Enabled signed cookie support.')
+  .options('title', {
+    describe: 'Name of server instance.',
+    default: 'WeeCloud'
+  })
+  .options('port', {
+    describe: 'Port.',
+    alias: 'P',
+    default: 8000
+  })
+  .options('verbose', {
+    describe: 'Display more verbose.',
+    alias: 'v',
+    boolean: true,
+    default: false
+  })
+  .options('help', {
+    describe: 'Display the usage.',
+    alias: 'h'
+  })
+
+var argv = yargs.argv;
 
 if (argv.help) {
-    optimist.showHelp();
-    process.exit(0);
+  yargs.showHelp();
+  process.exit(0);
 }
 
-var routes = function (app) {
-  console.log(app);
-  console.log(app.get);
-  app.get('/', function() {
-    return 'KINDA WORKS!';
-  });
-};
+if (argv.verbose) {
+  console.log = log;
+  console.warn = warn;
+}
+
+
+//
+// --- AUTHENTICATION (PASSPORT) ---
+//
+
 
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(function(username, password, done) {
-  if (username === 'garbas' &&
-      password === 'rokrok3') {
+  if (argv.username === username && argv.password === password) {
+      console.log('Logged in.');
       return done(null, username);
-  } 
+  }
+  console.warn('Wrong attempt: ' + username + '/' + password);
   return done(null, false, { message: 'Incorrect.' });
 }));
+
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -40,99 +89,88 @@ passport.deserializeUser(function(user, done) {
 });
 
 
+//
+// --- MIDDLEWARE (EXPRESS) ---
+//
 
-var path = require('path'),
-    connect = require('connect'),
-    flash = require('connect-flash'),
-    redirect = require('connect-redirection'),
-    connectRoute = require('connect-route'),
-    app = connect();
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    flash = require('express-flash'),
+    app = express();
 
 app
-  .use('/bower_components', connect.static(__dirname + '/bower_components'))
-  .use(connect.query())
-  .use(connect.bodyParser())
-  .use(connect.cookieParser())
-  .use(connect.session({ secret: 'whodunnit', cookie: { maxAge: 60000 }}))
-  .use(redirect())
+  .set('view engine', 'jade')
+  .use('/bower_components', express.static(__dirname + '/bower_components'))
+  .use('/static', express.static(__dirname + '/static'))
+  .use(bodyParser())
+  .use(cookieParser(argv['cookie-secret']))
+  .use(session({
+    secret: Math.random().toString() + (new Date()).valueOf().toString(),
+    cookie: { maxAge: 60000 }
+  }))
   .use(flash())
   .use(passport.initialize())
-  .use(passport.session())
-  .use(connectRoute(function(route) {
+  .use(passport.session());
 
-    route.get('/', function(request, response, next) {
-      if (!request.user) {
-        response.redirect('/login');
-        return;
-      }
+app.locals.title = argv.title;
 
-      try {
 
-        var output = '' +
-          '<!doctype html>' +
-          '<html>' +
-          '<head><title>WeeCloud - Login</title></title></head>' +
-          '<body>'+
-          '<h3>Logged in.</h3>' +
-          '<p>Hello ' + request.user + '.</h3>' +
-          '<p><a href="/logout">Logout</a></p>' +
-          '</body>' +
-          '</html>';
+//
+//  --- ROUTES ---
+//
 
-        response.writeHead(200, {
-          'Content-Length': output.length,
-          'Content-Encoding': 'utf-8',
-          'Content-Type': 'text/html'
-        });
-
-        response.end(output);
-
-      } catch (e) {
-        return next(e);
-      }
-    });
-
-    route.get('/login', function(request, response, next) {
-      if (request.user) {
-        response.redirect('/');
-      }
-
-      var output = '' +
-        '<!doctype html>' +
-        '<html>' +
-        '<head><title>WeeCloud - Login</title></title></head>' +
-        '<body><h3>' + request.flash('error') +'</h3>' +
-        '<form action="/login" method="post"><div><label>Username:</label><input type="text" name="username"/></div><div><label>Password:</label><input type="password" name="password"/></div><div><input type="submit" value="Log In"/></div></form>' +
-        '</body>' +
-        '</html>';
-
-      response.writeHead(200, {
-        'Content-Length': output.length,
-        'Content-Encoding': 'utf-8',
-        'Content-Type': 'text/html'
-      });
-
-      response.end(output);
-    });
-
-    route.post('/login', passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureFlash: true
-    }));
-
-    route.get('/logout', function(req, res){
-      req.logout();
-      res.redirect('/');
-    });
-
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
+    ensureLoggedOut = require('connect-ensure-login').ensureLoggedOut;
+    
+// login
+app.route('/login')
+  .get(
+    ensureLoggedOut('/'),
+    function(req, res) {
+      res.render('login', { messages: req.flash('error') });
+    })
+  .post(passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
   }));
 
+// dashboard
+app.route('/')
+  .get(
+    ensureLoggedIn('/login'),
+    function(req, res) {
+      res.render('dashboard', { user: req.user });
+    });
 
-var http = require('http');
+// chat
+app.route('/chat')
+  .get(
+    ensureLoggedIn('/login'),
+    function(req, res) {
+      console.log('chatting');
+    });
 
-http
-  .createServer(app)
+
+
+//
+// --- SERVER ---
+//
+
+var server = require('http').createServer(app),
+   io = require('socket.io').listen(server);
+
+server
   .listen(argv.port, function() {
-    console.log('Point your browser at http://localhost:' + argv.port);
+    console.log(argv.title + ' started at: http://localhost:' + argv.port);
   });
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
---
+++ static/main.js
@@ -0,0 +1,5 @@
+
+require([
+], function() {
+  alert('WORKS!');
+});
