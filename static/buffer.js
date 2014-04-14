71d30e39e342575a663d3a35dea8a99ca30cc94e
Domen Kožar <domen@dev.si>
2014-04-15 00:16:20 +0200

    add horrible amounts of code NOT to display colours.

--- static/buffer.js
+++ static/buffer.js
@@ -3,13 +3,11 @@ if (typeof define !== 'function') { var define = require('amdefine')(module) }
 define([
   'react',
   'moment',
-  './nicknamelist'
-], function(React, moment, NicknameList, undefined) {
+  './nicknamelist',
+  './color'
+], function(React, moment, NicknameList, Color, undefined) {
     
   // TODO: transform opacity when adding a new buffer line
-  function parseColor(text) {
-      return text;
-  }
 
   var Buffer = React.createClass({
 
@@ -37,24 +35,31 @@ define([
     render: function() {
       var buffer = this.props.buffer,
           name = buffer.info.short_name;
+      // TODO: when updating topic, short_name has a funny name 
       console.log("Buffer", this.props.buffer);
       return React.DOM.div({className: "row " + (this.props.isActive ? "" : " hidden")}, [
-          //React.DOM.h3({ key: 'title' }, name),
-          React.DOM.ul({className: "list-group col-lg-10",
-                       key: "listgroup",
-                       style: {"overflow-y": "auto",
-                               "overflow-x": "hidden",
-                               "height": 500}}, 
-             buffer.messages.map(function (message) {
-                 return React.DOM.li({className: "list-group-item row" + (message.highlight === 1 ? " list-group-item-info" : ""),
-                                      key: message.pointers.join(" ")}, [
-                     React.DOM.div({className: "col-sm-1 text-muted", key: "date"},
-                                   moment.unix(parseInt(message.date)).format('hh:mm:ss')),
-                     React.DOM.div({className: "col-sm-1 text-primary", key: "prefix"}, parseColor(message.prefix)),
-                     React.DOM.div({className: "col-sm-10", key: "message"}, parseColor(message.message))
-                 ]);
-             })
-          ),
+          React.DOM.div({className: "list-group col-lg-10"}, [
+            React.DOM.h3({ key: 'title' }, name + ": " + buffer.info.title),
+            React.DOM.ul({
+                         key: "listgroup",
+                         style: {"overflow-y": "auto",
+                                 "overflow-x": "hidden"}}, 
+               buffer.messages.map(function (message) {
+                   return React.DOM.li({className: "list-group-item row" + (message.highlight === 1 ? " list-group-item-info" : ""),
+                                        key: message.pointers.join(" ")}, [
+                       React.DOM.div({className: "col-sm-1 text-muted", key: "date"},
+                                     moment.unix(parseInt(message.date)).format('hh:mm:ss')),
+                       React.DOM.div({className: "col-sm-1 " + (message.tags_array.indexOf("irc_privmsg") === -1 ? "text-muted" : "text-primary"),
+                                      key: "prefix",
+                                      dangerouslySetInnerHTML: {__html: Color.format(message.prefix)}}),
+                       React.DOM.div({className: "col-sm-10 " + (message.tags_array.indexOf("irc_privmsg") === -1 ? "text-muted" : ""),
+                                      key: "message",
+                                      dangerouslySetInnerHTML: {__html: Color.format(message.message)}}
+                       )
+                   ]);
+               })
+            ),
+          ]),
           NicknameList({nicklist: this.props.buffer.nicklist})
         ]);
     }
